3f8322d6b853ec0564dbd0ac8adf0bc233f68edb
rajankit3107 <e22cseu0331@bennett.edu.in>
2025-07-01 23:31:05 +0530

    completed the models for mongodb

---
+++ src/utils/async-handler.js
@@ -0,0 +1,9 @@
+//we might need it later
+
+// const asyncHandler = (requestHandler) => {
+//   return (req, res, next) => {
+//     Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
+//   };
+// };
+
+// export { asyncHandler };
